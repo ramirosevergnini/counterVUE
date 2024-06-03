d2c9614843af63fff98538ecc6f7c25dda2fb573
Ramiro Severgnini <160265951+ramirosevergnini@users.noreply.github.com>
2024-06-03 15:20:59 -0300

    pronto

---
+++ counter/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+  import {ref} from "vue";
+  const count = ref(0)
+  const addCount = () => {
+    count.value += 1;
+  }
+  const substractCount = () => {
+    count.value -= 1;
+  }
+</script>
+
+
+<template>
+  <main>
+    <div>
+      <h4>The current count is...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="substractCount">-</button>
+      <button @click="addCount">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main{
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
+</style>
